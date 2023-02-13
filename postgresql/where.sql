4977376e52932ac1fbd98e59c54d298a48715de0
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-02-13 19:42:17 +0000

    Final test for WHERE

--- postgresql/where.sql
+++ postgresql/where.sql
@@ -19,3 +19,6 @@ AND customer_id = 426;
 SELECT payment_id, amount
 FROM payment
 WHERE amount BETWEEN '2020-01-24 00:00' AND '2020-01-26 00:00';
+
+SELECT * FROM customer
+WHERE customer_id IN (123,212,323,243,353,432);
