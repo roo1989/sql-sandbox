0e2f02dd55fd3c7d774d1ee82449aaea0e3a0d84
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-02-14 22:09:31 +0000

    Another challenge down

--- postgresql/where_challenge.sql
+++ postgresql/where_challenge.sql
@@ -37,3 +37,8 @@ SELECT * FROM payment
 WHERE customer_id IN (12,25,67,93,124,234)
 AND amount IN (4.99,7.99,9.99)
 AND payment_date BETWEEN '2019-01-01' AND '2020-02-01';
+
+SELECT DISTINCT COUNT(*) FROM customer
+WHERE first_name LIKE '___'
+AND (last_name LIKE '%X'
+OR last_name LIKE '%Y');
