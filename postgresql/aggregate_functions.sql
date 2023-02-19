ce213b44203166141b0e6738ff19cc9a0b5c235d
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-02-19 20:04:54 +0000

    introduce the HAVING keyword to the aggregate function library

--- postgresql/aggregate_functions.sql
+++ postgresql/aggregate_functions.sql
@@ -36,3 +36,10 @@ SELECT *,
        DATE(payment_date)
 FROM payment;
 
+-- Select a range using HAVING
+SELECT
+    customer_id,
+    SUM(amount)
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > 200;
