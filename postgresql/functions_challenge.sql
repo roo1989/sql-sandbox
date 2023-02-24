844c9cca3bd281b44f4657f6bf3ad20d4f3eedf2
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-02-24 18:37:49 +0000

    Position challenge down

--- postgresql/functions_challenge.sql
+++ postgresql/functions_challenge.sql
@@ -43,6 +43,21 @@ FROM customer;
   with three (3) stars (*) and then @email.com
  */
 
- SELECT
+SELECT
      LEFT(email, 1) || '***' || RIGHT(email, 19)
+FROM customer;
+
+/**
+  Challenge 4.
+
+  In this challenge, you have only the amil address and the last
+  name of the customers.
+
+  You need to extract the first name from the email address and
+  concatenate it with the last name. It should be in the form:
+  "Last name, First name"
+ */
+
+SELECT
+    last_name || ', ' || LEFT(email,POSITION('.' IN email)-1) AS name
 FROM customer;
